ae8a3934d051c4f88583ea61937d6255e0e4d9a0
takanori-matsumoto-mulodo <takanori.matsumoto@mulodo.com>
2014-12-25 21:26:58 +0900

    Add class file.

---
+++ EditableImage/EditableImageView.swift
@@ -0,0 +1,21 @@
+//
+//  EditableImageView.swift
+//  EditableImage
+//
+//  Created by TakanoriMatsumoto on 2014/12/25.
+//  Copyright (c) 2014年 TakanoriMatsumoto. All rights reserved.
+//
+
+import UIKit
+
+class EditableImageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
